77fe14db27bce69572291e621558493d98ebd9d0
Amanda Li <amli7@comcast.net>
2019-04-03 20:04:17 -0400

    builds and runs docker container from command line

--- containerize.sh
+++ containerize.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# run by "./containerize <dir_path>"
+# run by "./containerize <dir_path> <container_img>"
 
 echo "starting ..."
 python filespawn.py $1
 cd $1
+docker build -t $2 .
+echo "running"
+docker run -dit $2
+
+# TODO: store container id to execute
+# echo ${container_id}
+# docker exec -it container_id /bin/bash
 
 echo "done"
